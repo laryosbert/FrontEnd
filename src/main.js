7e9a59213c89435bf350e10e2b856b546d21b309
Larry <laryosbert@163.com>
2018-04-21 23:38:15 +0800

    add fetch

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+Vue.use(VueMaterial)
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
